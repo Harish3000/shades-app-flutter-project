51cb583b69b1d009a62a17be498d20af483e405d
Harish <129611646+Harish3000@users.noreply.github.com>
2023-10-18 16:25:07 +0530

    like button

--- shades/lib/features/query_mgt/querypage.dart
+++ shades/lib/features/query_mgt/querypage.dart
@@ -1,7 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-class ModuleDetailPage extends StatelessWidget {
+class ModuleDetailPage extends StatefulWidget {
   final String queryName;
   final String queryCode;
   final String description;
@@ -15,9 +15,23 @@ class ModuleDetailPage extends StatelessWidget {
   });
 
   @override
-  Widget build(BuildContext context) {
-    final TextEditingController answerController = TextEditingController();
+  _ModuleDetailPageState createState() => _ModuleDetailPageState();
+}
 
+class _ModuleDetailPageState extends State<ModuleDetailPage> {
+  late TextEditingController answerController;
+  late List<Map<String, dynamic>> answers;
+
+  @override
+  void initState() {
+    super.initState();
+    answerController = TextEditingController();
+    answers = [];
+    _getAndSortAnswers();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(
@@ -29,8 +43,7 @@ class ModuleDetailPage extends StatelessWidget {
             color: Colors.white,
           ),
         ),
-        backgroundColor:
-            Color.fromARGB(255, 2, 4, 10), // Choose your app's primary color
+        backgroundColor: Color.fromARGB(255, 2, 4, 10),
       ),
       body: SingleChildScrollView(
         padding: const EdgeInsets.all(16.0),
@@ -59,22 +72,22 @@ class ModuleDetailPage extends StatelessWidget {
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         Text(
-                          'Query Name: $queryName',
+                          'Query Name: ${widget.queryName}',
                           style: TextStyle(
                             fontSize: 20,
                             fontWeight: FontWeight.bold,
                           ),
                         ),
                         Text(
-                          'time stamp: $queryCode',
+                          'time stamp: ${widget.queryCode}',
                           style: TextStyle(fontSize: 16),
                         ),
                         Text(
-                          'Description: $description',
+                          'Description: ${widget.description}',
                           style: TextStyle(fontSize: 16),
                         ),
                         Text(
-                          'Tags: $tags',
+                          'Tags: ${widget.tags}',
                           style: TextStyle(fontSize: 16),
                         ),
                       ],
@@ -88,80 +101,7 @@ class ModuleDetailPage extends StatelessWidget {
               'Answers:',
               style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
             ),
-            StreamBuilder(
-              stream: FirebaseFirestore.instance
-                  .collection('query_answers')
-                  .where('queryCode', isEqualTo: queryCode)
-                  .snapshots(),
-              builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return CircularProgressIndicator();
-                }
-                if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
-                  return Text('No answers available.');
-                }
-                return Column(
-                  children: snapshot.data!.docs.map((answerData) {
-                    final answerId = answerData.id;
-                    final userName = answerData['userName'];
-                    final answer = answerData['answer'];
-                    final likes = answerData['likes'] ?? 0;
-                    final dislikes = answerData['dislikes'] ?? 0;
-
-                    return Card(
-                      elevation: 2,
-                      color: Colors.white,
-                      margin: EdgeInsets.symmetric(vertical: 8),
-                      child: Column(
-                        children: [
-                          ListTile(
-                            title: Text('Answer by $userName'),
-                            subtitle: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text('Answer: $answer'),
-                                Row(
-                                  children: [
-                                    IconButton(
-                                      icon: Icon(Icons.favorite,
-                                          color: Color.fromARGB(
-                                              255, 124, 117, 117)),
-                                      onPressed: () {
-                                        FirebaseFirestore.instance
-                                            .collection('query_answers')
-                                            .doc(answerId)
-                                            .update({
-                                          'likes': likes + 1,
-                                        });
-                                      },
-                                    ),
-                                    Text('Likes: $likes'),
-                                    IconButton(
-                                      icon: Icon(Icons.heart_broken,
-                                          color: Color.fromARGB(
-                                              255, 124, 117, 117)),
-                                      onPressed: () {
-                                        FirebaseFirestore.instance
-                                            .collection('query_answers')
-                                            .doc(answerId)
-                                            .update({
-                                          'dislikes': dislikes + 1,
-                                        });
-                                      },
-                                    ),
-                                    Text('Dislikes: $dislikes'),
-                                  ],
-                                ),
-                              ],
-                            ),
-                          ),
-                        ],
-                      ),
-                    );
-                  }).toList(),
-                );
-              },
-            ),
+            _buildAnswers(),
             SizedBox(height: 16),
             Text(
               'Add an Answer:',
@@ -179,8 +119,7 @@ class ModuleDetailPage extends StatelessWidget {
                       controller: answerController,
                       decoration: InputDecoration(
                         labelText: 'Answer',
-                        prefixIcon:
-                            Icon(Icons.comment), // Icon for the Answer field
+                        prefixIcon: Icon(Icons.comment),
                         border: OutlineInputBorder(),
                       ),
                     ),
@@ -190,17 +129,7 @@ class ModuleDetailPage extends StatelessWidget {
                         final String answer = answerController.text;
 
                         if (answer.isNotEmpty) {
-                          FirebaseFirestore.instance
-                              .collection('query_answers')
-                              .add({
-                            'queryName': queryName,
-                            'queryCode': queryCode,
-                            'userName': 'Anonymous',
-                            'answer': answer,
-                            'likes': 0,
-                            'dislikes': 0,
-                          });
-
+                          _submitAnswer(answer);
                           answerController.clear();
                         } else {
                           ScaffoldMessenger.of(context).showSnackBar(
@@ -230,5 +159,110 @@ class ModuleDetailPage extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  Widget _buildAnswers() {
+    return Column(
+      children: answers.map((answerData) {
+        final answerId = answerData['answerId'];
+        final userName = answerData['userName'];
+        final answer = answerData['answer'];
+        final likes = answerData['likes'] ?? 0;
+        final dislikes = answerData['dislikes'] ?? 0;
+
+        return Card(
+          key: ValueKey<String>(answerId),
+          elevation: 2,
+          color: Colors.white,
+          margin: EdgeInsets.symmetric(vertical: 8),
+          child: Column(
+            children: [
+              ListTile(
+                title: Text('Answer by $userName'),
+                subtitle: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Answer: $answer'),
+                    Row(
+                      children: [
+                        IconButton(
+                          icon: Icon(Icons.favorite,
+                              color: Color.fromARGB(255, 124, 117, 117)),
+                          onPressed: () {
+                            _updateLikes(answerId, likes + 1);
+                          },
+                        ),
+                        Text('Likes: $likes'),
+                        IconButton(
+                          icon: Icon(Icons.heart_broken,
+                              color: Color.fromARGB(255, 124, 117, 117)),
+                          onPressed: () {
+                            _updateDislikes(answerId, dislikes + 1);
+                          },
+                        ),
+                        Text('Dislikes: $dislikes'),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        );
+      }).toList(),
+    );
+  }
+
+  Future<void> _getAndSortAnswers() async {
+    final QuerySnapshot<Map<String, dynamic>> snapshot = await FirebaseFirestore
+        .instance
+        .collection('query_answers')
+        .where('queryCode', isEqualTo: widget.queryCode)
+        .get();
+
+    setState(() {
+      answers = snapshot.docs.map((doc) {
+        return {
+          'answerId': doc.id,
+          'userName': doc['userName'],
+          'answer': doc['answer'],
+          'likes': doc['likes'] ?? 0,
+          'dislikes': doc['dislikes'] ?? 0,
+        };
+      }).toList();
+
+      answers.sort((a, b) => (b['likes'] as int).compareTo(a['likes'] as int));
+    });
+  }
+
+  void _updateLikes(String answerId, int newLikes) async {
+    await FirebaseFirestore.instance
+        .collection('query_answers')
+        .doc(answerId)
+        .update({'likes': newLikes});
+
+    _getAndSortAnswers();
+  }
+
+  void _updateDislikes(String answerId, int newDislikes) async {
+    await FirebaseFirestore.instance
+        .collection('query_answers')
+        .doc(answerId)
+        .update({'dislikes': newDislikes});
+
+    _getAndSortAnswers();
+  }
+
+  void _submitAnswer(String answer) async {
+    await FirebaseFirestore.instance.collection('query_answers').add({
+      'queryName': widget.queryName,
+      'queryCode': widget.queryCode,
+      'userName': 'Anonymous',
+      'answer': answer,
+      'likes': 0,
+      'dislikes': 0,
+    });
+
+    _getAndSortAnswers();
   }
 }
