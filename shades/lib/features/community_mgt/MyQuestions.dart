b5b82b0a472f1bdc99b9decef64e1acc8605ba16
Harish <129611646+Harish3000@users.noreply.github.com>
2023-10-27 20:38:06 +0530

    added proper design

--- shades/lib/features/community_mgt/MyQuestions.dart
+++ shades/lib/features/community_mgt/MyQuestions.dart
@@ -49,46 +49,84 @@ class MyQuestions extends StatelessWidget {
                     borderRadius: BorderRadius.circular(15),
                     side: BorderSide(width: 3, color: Colors.black),
                   ),
-                  child: ListTile(
-                    contentPadding: EdgeInsets.all(16),
-                    leading: Image.asset(
-                      'assets/query/query2.png',
-                      width: 60,
-                      height: 60,
-                    ),
-                    title: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          documentSnapshot['queryName'].toString(),
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 19,
-                          ),
+                  child: Column(
+                    children: [
+                      ListTile(
+                        contentPadding: EdgeInsets.all(16),
+                        title: Row(
+                          children: [
+                            Image.asset(
+                              'assets/query/query3.png',
+                              width: 60,
+                              height: 60,
+                            ),
+                            SizedBox(width: 10),
+                            Flexible(
+                              child: Text(
+                                documentSnapshot['queryName'].toString(),
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 19,
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
-                        SizedBox(height: 30),
-                        Text(
-                          "- posted on : ${_formatDate(documentSnapshot['queryCode'].toString())} -",
-                          style: TextStyle(
-                            color: Color.fromARGB(201, 107, 107, 107),
-                            fontSize: 11,
-                          ),
+                        subtitle: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            SizedBox(height: 10),
+                            Text(
+                              "- posted on : ${_formatDate(documentSnapshot['queryCode'].toString())} -",
+                              style: TextStyle(
+                                color: Color.fromARGB(201, 107, 107, 107),
+                                fontSize: 11,
+                              ),
+                            ),
+                            SizedBox(height: 10),
+                            Text(
+                              "Tags: ${documentSnapshot['tags'].toString()}",
+                              style: TextStyle(
+                                fontSize: 12,
+                                color: Colors.grey,
+                              ),
+                            ),
+                          ],
                         ),
-                      ],
-                    ),
-                    onTap: () {
-                      Navigator.of(context).push(
-                        MaterialPageRoute(
-                          builder: (context) => ModuleDetailPage(
-                            queryName: documentSnapshot['queryName'].toString(),
-                            queryCode: documentSnapshot['queryCode'].toString(),
-                            description:
-                                documentSnapshot['description'].toString(),
-                            tags: documentSnapshot['tags'].toString(),
+                        onTap: () {
+                          Navigator.of(context).push(
+                            MaterialPageRoute(
+                              builder: (context) => ModuleDetailPage(
+                                queryName:
+                                    documentSnapshot['queryName'].toString(),
+                                queryCode:
+                                    documentSnapshot['queryCode'].toString(),
+                                description:
+                                    documentSnapshot['description'].toString(),
+                                tags: documentSnapshot['tags'].toString(),
+                              ),
+                            ),
+                          );
+                        },
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          IconButton(
+                            icon: Icon(Icons.edit),
+                            onPressed: () {
+                              _updateQuestion(context, documentSnapshot);
+                            },
                           ),
-                        ),
-                      );
-                    },
+                          IconButton(
+                            icon: Icon(Icons.delete),
+                            onPressed: () {
+                              _confirmDelete(context, documentSnapshot);
+                            },
+                          ),
+                        ],
+                      ),
+                    ],
                   ),
                 ),
               );
@@ -112,5 +150,116 @@ class MyQuestions extends StatelessWidget {
 
   String _formatNumber(int number) {
     return number < 10 ? '0$number' : '$number';
+  }
+
+  void _updateQuestion(
+      BuildContext context, DocumentSnapshot documentSnapshot) {
+    TextEditingController updatedQueryNameController =
+        TextEditingController(text: documentSnapshot['queryName'].toString());
+    TextEditingController updatedDescriptionController =
+        TextEditingController(text: documentSnapshot['description'].toString());
+    TextEditingController updatedTagsController =
+        TextEditingController(text: documentSnapshot['tags'].toString());
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("Update Question"),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(
+                controller: updatedQueryNameController,
+                maxLines: null, // Allows multiple lines
+                decoration:
+                    InputDecoration(labelText: "Enter Updated Query Name"),
+              ),
+              TextField(
+                controller: updatedDescriptionController,
+                maxLines: null,
+                decoration:
+                    InputDecoration(labelText: "Enter Updated Description"),
+              ),
+              TextField(
+                controller: updatedTagsController,
+                maxLines: null,
+                decoration: InputDecoration(labelText: "Enter Updated Tags"),
+              ),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text("Cancel"),
+            ),
+            TextButton(
+              onPressed: () {
+                _performUpdate(
+                  documentSnapshot,
+                  updatedQueryNameController.text,
+                  updatedDescriptionController.text,
+                  updatedTagsController.text,
+                );
+                Navigator.of(context).pop();
+              },
+              child: Text("Update"),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _performUpdate(
+    DocumentSnapshot documentSnapshot,
+    String updatedQueryName,
+    String updatedDescription,
+    String updatedTags,
+  ) {
+    FirebaseFirestore.instance
+        .collection('query')
+        .doc(documentSnapshot.id)
+        .update({
+      'queryName': updatedQueryName,
+      'description': updatedDescription,
+      'tags': updatedTags,
+    });
+  }
+
+  void _confirmDelete(BuildContext context, DocumentSnapshot documentSnapshot) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("Confirm Delete"),
+          content: Text("Are you sure you want to delete this question?"),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text("Cancel"),
+            ),
+            TextButton(
+              onPressed: () {
+                _deleteQuestion(documentSnapshot);
+                Navigator.of(context).pop();
+              },
+              child: Text("Delete"),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _deleteQuestion(DocumentSnapshot documentSnapshot) {
+    FirebaseFirestore.instance
+        .collection('query')
+        .doc(documentSnapshot.id)
+        .delete();
   }
 }
